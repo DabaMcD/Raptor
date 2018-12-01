c347151556ea50c1bc450511bf8f5e3c7ef1d152
gooberbobber <dabamcdoodle@gmail.com>
2018-12-01 15:28:49 -0300

    Create initializeGame function

--- app/src/main/java/com/raptor/raptor/raptor/GameScreen.java
+++ app/src/main/java/com/raptor/raptor/raptor/GameScreen.java
@@ -3,15 +3,17 @@ package com.raptor.raptor.raptor;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class GameScreen extends View {
-    private Paint paint;
     private Context context;
+    private Jet jet;
+    private ArrayList<Obstacle> b;
+    private int i;
 
     public GameScreen(Context context) {
         super(context);
@@ -27,15 +29,37 @@ public class GameScreen extends View {
     }
     private void constructor(Context context) {
         this.context = context;
-        paint = new Paint();
+        b = new ArrayList<>();
+        initializeGame();
     }
     @Override
     protected void onDraw(Canvas canvas) {
-
         super.onDraw(canvas);
     }
     void draw() {
         invalidate();
         requestLayout();
+    }
+    void initializeGame() {
+        jet = new Jet(Screen.width / 2, Screen.height - 100);
+
+        b.add(new Obstacle(roundRandom(1, 3), i));
+        i = 1;
+        b.add(new Obstacle(roundRandom(1, 3), i));
+        i = 2;
+        for (i = 2; -i * 250 > -Screen.height - 800; i++) {
+            b.add(new Obstacle(roundRandom(1, 4), i));
+        }
+    }
+    int roundRandom(int min, int max) {
+        if(min == max) {
+            return min;
+        }
+        if(min > max) {
+            int bob = max;
+            max = min;
+            min = bob;
+        }
+        return (int) Math.round((Math.random() * (max - min)) + min);
     }
 }
